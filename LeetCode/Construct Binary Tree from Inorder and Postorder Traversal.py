a23c302ac5a66486194302c504a025aa92adf13c
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2021-11-22 09:02:03 +0530

    Daily_Coding

---
+++ LeetCode/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -0,0 +1,49 @@
+"""
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+Example 1:
+
+
+Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: inorder = [-1], postorder = [-1]
+Output: [-1]
+ 
+
+Constraints:
+
+1 <= inorder.length <= 3000
+postorder.length == inorder.length
+-3000 <= inorder[i], postorder[i] <= 3000
+inorder and postorder consist of unique values.
+Each value of postorder also appears in inorder.
+inorder is guaranteed to be the inorder traversal of the tree.
+postorder is guaranteed to be the postorder traversal of the tree.
+"""
+
+# Solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        # infer from the way inorder and postorder traverse
+        def recur(inorder):
+            if len(postorder):
+                val = postorder[-1]
+                root =TreeNode(val)
+                if val not in inorder:
+                    return None
+                position = inorder.index(val)
+                postorder.pop()  # killing postorder
+                root.right=recur(inorder[position+1:])
+                root.left=recur(inorder[:position])
+                return root
+        return recur(inorder)
