5788d31efefce4afc44fb684311f2c2619cf07f8
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2021-11-23 09:03:59 +0530

    Daily_Coding

---
+++ LeetCode/ZigZag_Conversion.py
@@ -0,0 +1,52 @@
+"""
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+ 
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
+"""
+
+# Solution
+class Solution:
+    def convert(self, s: str,n: int) -> str:
+        if n<=1:
+            return s
+        res=[""]*n
+        curr, down = 0,0
+        for l in s:
+            res[curr]+= l
+            if curr==0 or curr==n-1:
+                down^=1
+            curr += 1 if down else -1 
+        return ''.join(res)
