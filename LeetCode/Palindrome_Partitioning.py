dfc43944e932408ebeadbb3102a5cd2543561221
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2022-01-05 15:11:13 +0530

    Daily_Coding

---
+++ LeetCode/Palindrome_Partitioning.py
@@ -0,0 +1,39 @@
+"""
+Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+A palindrome string is a string that reads the same backward as forward.
+
+ 
+
+Example 1:
+
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+Example 2:
+
+Input: s = "a"
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= s.length <= 16
+s contains only lowercase English letters.
+"""
+
+# Solution
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def dfs(s,path,res):
+            if not s:
+                res.append(path[:])
+                return
+            for i in range(1,len(s)+1):
+                if s[:i]==s[i-1::-1]:
+                    path.append(s[:i])
+                    dfs(s[i:],path,res)
+                    path.pop()
+        res=[]
+        dfs(s,[],res)
+        return res
+    
