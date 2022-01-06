e48cbfa98e2f9fead0b88bfb4872607df7b02c87
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2022-01-06 13:33:28 +0530

    Daily_Coding

---
+++ LeetCode/Car_Pooling.py
@@ -0,0 +1,44 @@
+"""
+There is a car with capacity empty seats. The vehicle only drives east (i.e., it cannot turn around and drive west).
+
+You are given the integer capacity and an array trips where trip[i] = [numPassengersi, fromi, toi] indicates that the ith trip has numPassengersi passengers and the locations to pick them up and drop them off are fromi and toi respectively. The locations are given as the number of kilometers due east from the car's initial location.
+
+Return true if it is possible to pick up and drop off all passengers for all the given trips, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: trips = [[2,1,5],[3,3,7]], capacity = 4
+Output: false
+
+
+Example 2:
+
+Input: trips = [[2,1,5],[3,3,7]], capacity = 5
+Output: true
+ 
+
+Constraints:
+
+1 <= trips.length <= 1000
+trips[i].length == 3
+1 <= numPassengersi <= 100
+0 <= fromi < toi <= 1000
+1 <= capacity <= 105
+"""
+
+# Solution
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        l = []
+        for n, start, end in trips:
+            l.append((start,n))
+            l.append((end,-n))
+        l.sort()
+        p = 0
+        for location in l:
+            p += location[1]
+            if p > capacity:
+                return False
+        return True
