bc55b6bba1f6a0211725470297aefdf129d8e277
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2022-01-28 17:41:35 +0530

    daily_coding

---
+++ LeetCode/Design Add and Search Words Data Structure.py
@@ -0,0 +1,60 @@
+"""
+Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+Implement the WordDictionary class:
+
+WordDictionary() Initializes the object.
+void addWord(word) Adds word to the data structure, it can be matched later.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+ 
+
+Example:
+
+Input
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+Output
+[null,null,null,null,false,true,true,true]
+
+Explanation
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+wordDictionary.search("pad"); // return False
+wordDictionary.search("bad"); // return True
+wordDictionary.search(".ad"); // return True
+wordDictionary.search("b.."); // return True
+ 
+
+Constraints:
+
+1 <= word.length <= 500
+word in addWord consists lower-case English letters.
+word in search consist of  '.' or lower-case English letters.
+At most 50000 calls will be made to addWord and search.
+"""
+
+# Solution
+class WordDictionary:
+
+    def __init__(self):
+        self.trie = {}
+
+    def addWord(self, word: str) -> None:
+        curr = self.trie
+        for c in word:
+            curr = curr.setdefault(c,{})
+        curr['$']=None        
+
+    def search(self, word: str) -> bool:
+        nodes=[self.trie]
+        for c in word + '$':
+            nodes=[kid for node in nodes for kid in ([node[c]] if c in node else filter(None, node.values()) if c=='.' else [])]
+        return bool(nodes)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
