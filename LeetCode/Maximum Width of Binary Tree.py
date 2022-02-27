5994debfb2a2f2b5641241b0289d19ec8f3cb641
MUNSHI AMINUR RASHID <aminurrashid224@gmail.com>
2022-02-27 21:09:43 +0530

    Daily_coding

---
+++ LeetCode/Maximum Width of Binary Tree.py
@@ -0,0 +1,60 @@
+"""
+Given the root of a binary tree, return the maximum width of the given tree.
+
+The maximum width of a tree is the maximum width among all levels.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), where the null nodes between the end-nodes are also counted into the length calculation.
+
+It is guaranteed that the answer will in the range of 32-bit signed integer.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,3,2,5,3,null,9]
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+Example 2:
+
+
+Input: root = [1,3,null,5,3]
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+Example 3:
+
+
+Input: root = [1,3,2,5]
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 3000].
+-100 <= Node.val <= 100
+"""
+
+# Solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return []
+        width,lev=0,[(root,1)]
+        while len(lev):
+            width=max(width,lev[-1][1]-lev[0][1]+1)
+            next_lev=[]
+            for i,num in lev:
+                if i.left:
+                    next_lev.append((i.left,num*2))
+                if i.right:
+                    next_lev.append((i.right,num*2+1))
+            lev=next_lev
+        return width
